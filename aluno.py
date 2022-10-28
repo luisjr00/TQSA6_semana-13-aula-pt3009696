6ae14478a9b24bd311a2e5617aa6929f22f45536
Luís Antônio Nascimento Júnior <88392564+luisjr00@users.noreply.github.com>
2022-10-28 11:14:53 -0300

    first commit

---
+++ aluno.py
@@ -0,0 +1,9 @@
+class Aluno:
+  def __init__(self, nome, sobrenome, nota):
+    self.nome = nome;
+    self.sobrenome = sobrenome;
+    self.nota = nota;
+    
+
+  def mostrarAluno(self):
+    return 'Aluno: ' + self.nome + ' ' + self.sobrenome + ' - Nota: ' + str(self.nota);
